3ed239a6f7c6471d488fd4a6d89f0b820057dc42
Gas-prod <62751705+Gas-prod@users.noreply.github.com>
2021-03-23 17:05:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import tweepy
 
 def auth():
 
-    consumer_key = "your_consumer_key"
-    consumer_secret = "your_consumer_secret"
+    consumer_key = "consumer_key"
+    consumer_secret = "consumer_secret"
 
-    access_token = "your_access_token"
-    access_token_secret = "your_access_token_secret"
+    access_token = "access_token"
+    access_token_secret = "access_token_secret"
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
